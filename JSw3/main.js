d7bdfa27e9a1a0adc1734aea27cf5687971b9c1e
sriram005678 <sriram005678@gmail.com>
2021-12-17 16:13:23 +0530

    practices

---
+++ JSw3/main.js
@@ -0,0 +1,14 @@
+document.write(5+9); // for testing purpose
+
+// window.alert(5);// alert box
+
+var x = "5" + 2 + 3; // its concadetinate
+console.log(x);
+
+var x2 = 2 + 3 + "5";
+console.log(x2);
+
+var $$$ = "Hello World";
+console.log($$$);
+
+
